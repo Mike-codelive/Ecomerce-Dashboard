aadf34610bd2bd5c990759ed0e779882ce47b518
Miguel Ángel <txrz56rad@live.com.mx>
2024-10-30 23:22:18 -0600

    Project structure & Tailwind

---
+++ src/views/auth/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <>
+      <h1>Log in component</h1>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/views/auth/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+  return (
+    <>
+      <h1>Register component</h1>
+    </>
+  );
+}
+
+export default Register;
